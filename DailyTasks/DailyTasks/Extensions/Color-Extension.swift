286231a6e67eb307f06ad6efc76b1c6ed14b15d5
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-04-23 15:52:33 +0200

    Add color extension, theme color

---
+++ DailyTasks/DailyTasks/Extensions/Color-Extension.swift
@@ -0,0 +1,39 @@
+//
+//  Color-Extension.swift
+//  DailyTasks
+//
+//  Created by Matthias Zarzecki on 23.04.21.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  static let dailyHabitsColor = Color(hex: "b6d7a8")
+  
+  /// Creates a SwiftUI color from a hex code.
+  init(hex: String) {
+    let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+    var int: UInt64 = 0
+    Scanner(string: hex).scanHexInt64(&int)
+    let a, r, g, b: UInt64
+    switch hex.count {
+    case 3: // RGB (12-bit)
+      (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+    case 6: // RGB (24-bit)
+      (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+    case 8: // ARGB (32-bit)
+      (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+    default:
+      (a, r, g, b) = (1, 1, 1, 0)
+    }
+    
+    self.init(
+      .sRGB,
+      red: Double(r) / 255,
+      green: Double(g) / 255,
+      blue: Double(b) / 255,
+      opacity: Double(a) / 255
+    )
+  }
+}
