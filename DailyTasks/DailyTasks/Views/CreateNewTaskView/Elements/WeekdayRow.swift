13a23ffa28bdc75e7184100309deac54877b4b25
Matthias Zarzecki <matthias.zarzecki@chrono24.com>
2022-09-14 10:52:47 +0200

    Add weekdayRow class

---
+++ DailyTasks/DailyTasks/Views/CreateNewTaskView/Elements/WeekdayRow.swift
@@ -0,0 +1,59 @@
+//
+//  WeekdayRow.swift
+//  DailyTasks
+//
+//  Created by Matthias Zarzecki on 13.09.22.
+//
+
+import SwiftUI
+
+struct WeekdayRow: View {
+  @Binding var selectedPartOfDay: Int
+
+  private let padding: CGFloat = .spacing6
+
+  var body: some View {
+    HStack {
+      ForEach(PartOfDayOption.options, id: \.self) { option in
+        if selectedPartOfDay == option.index {
+          Text(option.name)
+            .padding(padding)
+            .backgroundColor(.white)
+            .foregroundColor(.gray)
+            .overlay(
+              RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.gray, lineWidth: 2)
+            )
+        } else {
+          Button(
+            action: {
+              selectedPartOfDay = option.index
+            },
+            label: {
+              Text("\(option.name)")
+                .padding(padding)
+                .backgroundColor(.gray)
+                .foregroundColor(.white)
+                .mask(
+                  RoundedRectangle(
+                    cornerRadius: 10,
+                    style: .continuous
+                  )
+                )
+            }
+          )
+        }
+      }
+    }
+    .padding(.spacing6)
+  }
+}
+
+struct WeekdayRow_Previews: PreviewProvider {
+  static var previews: some View {
+    PartOfDayRow(
+      selectedPartOfDay: .constant(1)
+    )
+    .previewLayout(.sizeThatFits)
+  }
+}
